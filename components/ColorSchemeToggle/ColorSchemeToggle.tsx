78db65513567fbe6fff37feb39f997e04ee02d89
Filippo Visconti <filippo.visconti23@gmail.com>
2024-02-20 11:09:50 +0100

    Add theme toggle, header components

--- components/ColorSchemeToggle/ColorSchemeToggle.tsx
+++ components/ColorSchemeToggle/ColorSchemeToggle.tsx
@@ -1,15 +1,34 @@
 'use client';
 
-import { Button, Group, useMantineColorScheme } from '@mantine/core';
+import { /* Button, */ Group, useMantineColorScheme, useComputedColorScheme } from '@mantine/core';
+
+import { ActionIcon } from '@mantine/core';
+import { SunIcon, MoonIcon } from '@modulz/radix-icons';
 
 export function ColorSchemeToggle() {
-  const { setColorScheme } = useMantineColorScheme();
+    const { /* setColorScheme, */ toggleColorScheme } = useMantineColorScheme({
+        keepTransitions: true,
+    });
+    const color = useComputedColorScheme('light');
 
-  return (
-    <Group justify="center" mt="xl">
-      <Button onClick={() => setColorScheme('light')}>Light</Button>
-      <Button onClick={() => setColorScheme('dark')}>Dark</Button>
-      <Button onClick={() => setColorScheme('auto')}>Auto</Button>
-    </Group>
-  );
+    return (
+        <Group justify="center" mt="xl">
+            {/* <Button onClick={() => setColorScheme('light')}>Light</Button>
+            <Button onClick={() => setColorScheme('dark')}>Dark</Button>
+            <Button onClick={() => setColorScheme('auto')}>Auto</Button> */}
+            <ActionIcon
+                variant="outline"
+                color={color === 'dark' ? 'yellow' : 'blue'}
+                onClick={() => toggleColorScheme()}
+                title="Toggle color scheme"
+            >
+                {color === 'dark' ? (
+                    <SunIcon style={{ width: 18, height: 18 }} />
+                ) : (
+                    <MoonIcon style={{ width: 18, height: 18 }} />
+                )}
+            </ActionIcon>
+
+        </Group>
+    );
 }
