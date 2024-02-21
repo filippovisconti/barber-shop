42bf59808cf3597f9f2381e2b8591143c41c79d4
Filippo Visconti <filippo.visconti23@gmail.com>
2024-02-21 16:55:13 +0100

    Add services list page

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,8 +1,9 @@
-import ServiceRepository from "../db/repositories/ServiceRepository";
-import { Service } from "../db/schema";
-import ServiceTable from "@/components/ServiceTable/ServiceTable";
-import { Container, Title } from '@mantine/core';
-
+import { Box, Center, Container, Group, Space, Title } from '@mantine/core';
+import ServiceRepository from '../db/repositories/ServiceRepository';
+import { Service } from '../db/schema';
+import ServiceTable from '@/components/ServiceTable/ServiceTable';
+import { EmailBanner } from '@/components/EmailBanner/EmailBanner';
+import { ImageActionBanner } from '@/components/ImageActionBanner/ImageActionBanner';
 
 async function getData() {
     const res = ServiceRepository.getAll();
@@ -10,15 +11,32 @@ async function getData() {
 }
 
 export default async function Page() {
-    let data = await getData();
-    let service_names: Service[] = JSON.parse(JSON.stringify(data)) as Service[];
+    const data = await getData();
+    const service_names: Service[] = JSON.parse(JSON.stringify(data)) as Service[];
+    const table_and_banner = (
+        <>
+            <Box miw={{base:200, sm: 500, md: 600}} py="20" mx="auto">
+                <ServiceTable service_names={service_names} />
+            </Box>
+            <Box maw={300} mx="auto">
+                <ImageActionBanner />
+            </Box>
+        </>);
 
     return <main>
         <Container>
-            <Title py='20' order={1}>
+            <Title py="20" order={1}>
                 List of services
             </Title>
-            <ServiceTable service_names={service_names} />
+            <Group justify="center" grow visibleFrom="sm">
+                {table_and_banner}
+            </Group>
+            <Box hiddenFrom="sm">
+                {table_and_banner}
+            </Box>
+            <Space h="xl" />
+            <EmailBanner />
         </Container>
-    </main>
+
+    </main>;
 }
