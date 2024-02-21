845d31444ed86ca14413cf813f91b1e2e005b7e1
Filippo Visconti <filippo.visconti23@gmail.com>
2024-02-21 16:55:29 +0100

    Insert location cards

--- app/locations/page.tsx
+++ app/locations/page.tsx
@@ -1,6 +1,6 @@
-import { Container } from '@mantine/core';
-import LocationRepository from '../db/repositories/LocationRepository';
 import LocationCards from '@/components/LocationCards/LocationCards';
+import { Center, Container, Title } from '@mantine/core';
+import LocationRepository from '../db/repositories/LocationRepository';
 import { Location } from '../db/schema';
 
 async function getData() {
@@ -11,16 +11,24 @@ export default async function Page() {
     const data = await getData();
     const locations: Location[] = JSON.parse(JSON.stringify(data)) as Location[];
     return (
-    <main>
-        <Container>
-            At [Barber Shop Name], we believe in the power of a great haircut to boost confidence and
-            leave a lasting impression. With a passion for precision and a commitment to customer
-            satisfaction, our team of skilled barbers is dedicated to providing an exceptional grooming
-            experience for every client. You can find us here!
+        <main>
+            <Container p='20'>
+                <Center>
+                    <Title item-center order={1}>Find us!</Title>
+                </Center>
+            </Container>
 
-            [Insert Map here]
+            <Container p='20'>
+                At [Barber Shop Name], we believe in the power of a great haircut to boost confidence
+                and leave a lasting impression. With a passion for precision and a commitment to
+                customer satisfaction, our team of skilled barbers is dedicated to providing an
+                exceptional grooming experience for every client. You can find us here!
 
-            <LocationCards locations={locations} />
-        </Container>
-    </main>);
-};
+                [Insert Map here]
+            </Container>
+
+            <Container py='30'>
+                <LocationCards locations={locations} />
+            </Container>
+        </main>);
+}
