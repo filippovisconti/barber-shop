821f7f4d7be48bfca3b53ea36d69251dac4cf42c
allegra <all.strippoli@stud.uniroma3.it>
2024-03-09 16:57:44 +0100

    feat: change date format

--- components/AppointmentTable/AppointmentTable.tsx
+++ components/AppointmentTable/AppointmentTable.tsx
@@ -1,24 +1,20 @@
 'use client'
 
 import { JoinAppointment } from '@/app/db/schema'
-import { Box, Table, Text } from '@mantine/core'
+import { Box, Table } from '@mantine/core'
+import moment from 'moment'
 import Link from 'next/link'
 
 export default function AppointmentTable(props: { appointments: JoinAppointment[] }) {
     const rows = props.appointments.map((element: JoinAppointment) => (
         <Table.Tr key={element.id}>
-            <Table.Td>
-                <Text fw={700}>
-                    <Link href={`/appointments/${element.id}`}>{element.id}</Link>
-                </Text>
-            </Table.Td>
             <Table.Td>{element.userEmail}</Table.Td>
             <Table.Td>{element.serviceName}</Table.Td>
             <Table.Td>{element.locationName}</Table.Td>
-            <Table.Td>{element.date}</Table.Td>
-            <Table.Td>{element.notes}</Table.Td>
-            <Table.Td>{element.createdAt}</Table.Td>
+            <Table.Td>{moment(element.date).format('dddd, MMMM Do YYYY, h:mm a')}</Table.Td>
+            <Table.Td>{moment(element.date).format('MM/DD/YYYY')}</Table.Td>
             <Table.Td>{element.status}</Table.Td>
+            <Table.Td>{element.notes == "" ? '-' : element.notes}</Table.Td>
         </Table.Tr>
     ))
 
@@ -27,14 +23,13 @@ export default function AppointmentTable(props: { appointments: JoinAppointment[
             <Table striped highlightOnHover>
                 <Table.Thead>
                     <Table.Tr>
-                        <Table.Th>Appointments</Table.Th>
                         <Table.Th>Email</Table.Th>
                         <Table.Th>Service</Table.Th>
                         <Table.Th>Location</Table.Th>
                         <Table.Th>Date</Table.Th>
-                        <Table.Th>Notes</Table.Th>
                         <Table.Th>Created at</Table.Th>
                         <Table.Th>Status</Table.Th>
+                        <Table.Th>Notes</Table.Th>
                     </Table.Tr>
                 </Table.Thead>
                 <Table.Tbody>{rows}</Table.Tbody>
